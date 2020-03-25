0e185594cc7cf07140ee31576e548d0d082ca787
yusuke-hub <yk1210140408@gmail.com>
2020-03-25 06:03:51 +0000

    残り時間を表示

--- Javascript/MyTypingGame/main.js
+++ Javascript/MyTypingGame/main.js
@@ -6,27 +6,35 @@
   let loc = 0;
   let score = 0;
   let miss = 0;
+  const timeLimit = 3 * 1000;
+  let startTime;
 
   const target = document.getElementById("target");
   const scoreLabel = document.getElementById("score");
   const missLabel = document.getElementById("miss");
+  const timerLabel = document.getElementById("timer");
 
   function updateTarget() {
     let placeholder = "";
     for (let i = 0; i < loc; i++) {
       placeholder += "_";
     }
-    // substring: 部分文字列を取得する
     target.textContent = placeholder + word.substring(loc);
+  }
+
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
   }
 
   window.addEventListener("click", () => {
     target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
   });
+
   window.addEventListener("keydown", e => {
-    console.log(e.key);
     if (e.key === word[loc]) {
-      console.log("score");
       loc++;
       if (loc === word.length) {
         word = words[Math.floor(Math.random() * words.length)];
@@ -36,7 +44,6 @@
       score++;
       scoreLabel.textContent = score;
     } else {
-      console.log("miss");
       miss++;
       missLabel.textContent = miss;
     }
