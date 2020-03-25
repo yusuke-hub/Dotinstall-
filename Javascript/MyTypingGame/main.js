80aa07c68b874212e6ed5eb4fdded14c7eb04832
yusuke-hub <yk1210140408@gmail.com>
2020-03-25 05:47:04 +0000

    複数の単語に対応させる

--- Javascript/MyTypingGame/main.js
+++ Javascript/MyTypingGame/main.js
@@ -1,7 +1,8 @@
 "use strict";
 
 {
-  const word = "apple";
+  const words = ["apple", "sky", "blue", "middle", "set"];
+  let word = words[Math.floor(Math.random() * words.length)];
   let loc = 0;
   let score = 0;
   let miss = 0;
@@ -11,11 +12,26 @@
   const missLabel = document.getElementById("miss");
 
   target.textContent = word;
+
+  function updateTarget() {
+    let placeholder = "";
+    for (let i = 0; i < loc; i++) {
+      placeholder += "_";
+    }
+    // substring: 部分文字列を取得する
+    target.textContent = placeholder + word.substring(loc);
+  }
+
   window.addEventListener("keydown", e => {
     console.log(e.key);
     if (e.key === word[loc]) {
       console.log("score");
       loc++;
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
+      updateTarget();
       score++;
       scoreLabel.textContent = score;
     } else {
